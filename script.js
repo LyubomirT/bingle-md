69414985cee8fa2bcfeba60ff787c8818bfa780c
LyubomirT <terlyubomir@gmail.com>
2023-03-30 15:12:13 +0300

    Delete unnecessary files

--- script.js
+++
@@ -1,8 +0,0 @@
-const md = window.markdownit();
-const input = document.getElementById("input");
-const output = document.getElementById("output");
-
-input.addEventListener("input", function() {
-  const result = md.render(input.value);
-  output.innerHTML = result;
-});
